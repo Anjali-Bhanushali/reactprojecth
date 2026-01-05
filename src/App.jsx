26395bdd5eca7715008e44681de327fb29eabb71
Anjali kumari <anjalisinghbhanushali@gmail.com>
2026-01-06 02:02:33 +0530

    Add Header component and update navigation UI
    Introduced a new Header component and integrated it into App.jsx. Updated the navigation bar with improved styling using Tailwind CSS. Set global body background to black and text to white in index.css. Fixed Tailwind config to include all subdirectories for content scanning.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import react from 'react'
+import Header from './components/Header'
 
 
 
@@ -7,7 +8,19 @@ function App() {
 
   return (
     <>
-      <h1>hello</h1>
+      <nav className="flex items-center justify-between px-10 py-4 bg-emerald-950 text-white">
+      {/* Logo */}
+      <h1 className="text-2xl font-bold">Anjali</h1>
+
+      {/* Menu */}
+      <ul className="flex gap-8 text-lg">
+        <li className="cursor-pointer hover:text-gray-200">Home</li>
+        <li className="cursor-pointer hover:text-gray-200">About</li>
+        <li className="cursor-pointer hover:text-gray-200">Projects</li>
+        <li className="cursor-pointer hover:text-gray-200">Contact</li>
+      </ul>
+    </nav>
+    <Header />
     </>
   )
 }
